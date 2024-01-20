0017eaf4f850dc3347299f24ab98ebbcfaac9e06
REHAN SHAH <spamhomeacc123@gmail.com>
2024-01-21 01:01:24 +0530

    Created login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
-import { BrowserRouter } from "react-router-dom"
+import { BrowserRouter,Route } from "react-router-dom"
 import {Navbar} from "./components/Navbar"
 import Hero from "./components/Hero"
+import { Login } from "./components/Login"
 
 function App() {
   return (
     <BrowserRouter>
-      <div className="bg bg-heroBG bg-cover h-[700px]">
+      
+      <div className="bg bg-heroBG bg-cover h-screen w-screen">
         <Navbar />
         <Hero />
       </div>
+        
+
+      
     </BrowserRouter>
   )
 }
